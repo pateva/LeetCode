7b9a599bbf08c7bb76ca7eb83d9a175d92caf1e6
pateva <83903221+pateva@users.noreply.github.com>
2023-05-07 16:59:22 +0300

    binary search with index

--- 2_Search_Insert_Position/src/main/Main.java
+++ 2_Search_Insert_Position/src/main/Main.java
@@ -5,18 +5,25 @@ public class Main {
     public static int searchInsert(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        int mid;
+        int mid = left + (right - left) / 2;
 
         while(left <= right) {
-            mid = left + (right - left) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
 
+            mid = left + (right - left) / 2;
         }
 
-
-        return 0;
+        return mid;
     }
 
     public static void main(String[] args) {
-        // write your code here
+        int nums[] = {1,3,5,6};
+        int target = 7;
+        System.out.println(searchInsert(nums, target));
     }
 }
