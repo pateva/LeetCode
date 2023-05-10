11b5ba4b4ab0233ed671a89e9e96fad1a2638439
pateva <83903221+pateva@users.noreply.github.com>
2023-05-10 09:24:18 +0300

    find the right interval

---
+++ 5_Find_Right_Interval/src/main/Main.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static int[] findRightInterval(int[][] intervals) {
+        int[] res = new int[intervals.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<intervals.length; i++) {
+            int[] interval = intervals[i];
+            max = Math.max(max, interval[0]);
+            map.put(interval[0], i);
+        }
+
+        for(int i=0; i<intervals.length; i++){
+            int end = intervals[i][1];
+            if(end > max) {
+                res[i] = -1;
+                continue;
+            }
+            int j = end;
+            while(!map.containsKey(j)) {
+                j++;
+            }
+            res[i] = map.get(j);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
